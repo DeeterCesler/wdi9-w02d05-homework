a979ba391a1fbad8b296a6141a25268fc55227c8
Deeter <deeter.cesler@gmail.com>
2018-09-23 12:49:17 -0600

    chapter 3 complete - made the ring and gave it to Frodo

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -96,9 +96,14 @@ const makeHobbits = () => {
 const keepItSecretKeepItSafe = () => {
 
   // 1. create an empty div with an id of 'the-ring'
-
+  const $theRing = $("<div id='the-ring'></div>");
+  console.log($theRing);
   // 2. add the ring as a child of Frodo
-
+  $(".hobbit").each(function(){
+    if($(this).text() === "Frodo Baggins"){
+    $(this).append($theRing);
+    }
+  });
   // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
 
   // when you think you have given Frodo the ring, check in your Elements tab
